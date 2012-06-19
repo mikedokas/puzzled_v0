87607ac6441e7059efe98237e9242a32c23bd64a
Mike <mikedokas@gmail.com>
2012-06-19 16:53:18 -0400

    test

---
+++ Puzzled_v0/Puzzled_v0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Puzzled_v0
+//
+//  Created by Michael Dokas on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
